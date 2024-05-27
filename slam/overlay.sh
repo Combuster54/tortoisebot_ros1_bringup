33dda1c90f07159af91bdf488ce31e90ea303aad
combuster <rubenovidalo@gmail.com>
2024-05-27 17:38:10 -0500

    m

--- slam/overlay.sh
+++ slam/overlay.sh
@@ -13,7 +13,6 @@ source /catkin_ws/carto_ws/devel_isolated/setup.bash
 # docker run -it --network host -e ROS_MASTER_URI=http://master:11311 -e ROS_IPV6=on  -e DISPLAY=:1  combuster54/tortoisebot:noetic-slam /bin/bash
 
 # exec
-d
 sleep 1s
 
 $@
