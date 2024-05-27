0ba954f56c46515f0c0d7e8c728dbe8e8e403a85
combuster <rubenovidalo@gmail.com>
2024-05-27 17:34:02 -0500

    m

--- slam/overlay.sh
+++ slam/overlay.sh
@@ -3,11 +3,17 @@ set -e
 
 # setup ros environment
 source /opt/ros/$ROS_DISTRO/setup.bash
-cd /catkin_ws/carto_ws
-source devel_isolated/setup.bash
-cd ..
-source devel/setup.bash
+#source /catkin_ws/devel/setup.bash
 
+# ejecutar 
+source /catkin_ws/carto_ws/devel_isolated/setup.bash
+# rosrun cartographer_ros cartographer_node -configuration_directory /catkin_ws/src/tortoisebot/tortoisebot_firmware/config -configuration_basename lidar.lua
+
+# Para contenedor
+# docker run -it --network host -e ROS_MASTER_URI=http://master:11311 -e ROS_IPV6=on  -e DISPLAY=:1  combuster54/tortoisebot:noetic-slam /bin/bash
+
+# exec
+d
 sleep 1s
 
 $@
